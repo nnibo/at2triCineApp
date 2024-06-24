39ab8b4cf53e5e13773e973a9fb21352b79b4c2f
João Vitor <joaovitorlh06@gmail.com>
2024-06-24 13:21:39 -0300

    atts

--- site-cineapp/src/Componentes/Header/Header.jsx
+++ site-cineapp/src/Componentes/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return ( 
         <header className="relative flex bg-black text-white pl-10 pt-9 pb-10 justify-between">
 
-            <img className="w-[125px]"  src="logo.png"/>
+            <img className="w-[125px]" src="logo.png"/>
         
             <div className="absolute left-1/2 transform -translate-x-1/2">
                 <Navbar/>
