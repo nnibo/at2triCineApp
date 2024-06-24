a84246196818ac57b4e895b1956c2a4b1ea17ea0
João Vitor <joaovitorlh06@gmail.com>
2024-06-24 13:36:48 -0300

    pasta public erro

--- site-cineapp/src/Pages/Home.jsx
+++ site-cineapp/src/Pages/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
                         <p className="text mb-3 text-gray-300">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a
                             galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
-                            the leap into electronic typesetting, remaining essentially unchanged.</p>
+                            the leap into electronic typesetting, remaining essentially.</p>
                     </div>
 
                 </div>
