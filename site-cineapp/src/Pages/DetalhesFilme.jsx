de603a52a445558c9c26481a4f969210bdfc93c0
João Vitor <joaovitorlh06@gmail.com>
2024-06-24 13:29:09 -0300

    a

--- site-cineapp/src/Pages/DetalhesFilme.jsx
+++ site-cineapp/src/Pages/DetalhesFilme.jsx
@@ -28,8 +28,7 @@ function DetalhesFilmes() {
                 </div>
 
                 <p className="text-gray-300 mt-5 mr-[100px]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a
-                          galley of type and scrambled it to make a type specimen book. </p>
+                         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took.</p>
             </div>
         </div>
         </> 
