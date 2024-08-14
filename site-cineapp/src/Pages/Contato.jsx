120b107df28b9cf2542d7ad87c3d0eae2e9c18d1
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-14 13:38:03 -0300

    telaContato

--- site-cineapp/src/Pages/Contato.jsx
+++ site-cineapp/src/Pages/Contato.jsx
@@ -20,9 +20,9 @@ function Contato() {
             message : mensagem
         }
 
-        emailjs.send('service_46oyt8a','template_gmi57ac',templateParams, '01V-CFd2eYAYMckOM')
+        emailjs.send('service_9vjvuyw','template_gmi57ac',templateParams, '01V-CFd2eYAYMckOM')
         .then((response) =>{
-            console.log('Email enviado com sucesso', respose.status, response.text);
+            console.log('Email enviado com sucesso', response.text);
             setName('')
             setEmail('')
             setMensagem('')
@@ -38,10 +38,48 @@ function Contato() {
         </div>
 
         <div id="container" className="w-full flex justify-center">
-        <div className="flex justify-center bg-black w-[600px] h-[400px] mt-10 rounded-3xl">
+        <div className="flex justify-center bg-black w-[1200px] h-[700px] mt-10 rounded-3xl">
 
+    <div className="flex flex-wrap justify-center items-center p-24 w-[550px]">
+        <div className=" flex flex-row ">
+            <div className="bg-white w-[180px] h-[220px] mr-10 rounded-xl flex items-center" >
+                <div className="flex flex-col items-center">
+                    <img className="w-10" src="../../public/localizacao.png" alt="" />
+                    <h2 className="text-center">Localizacao</h2>
+                    <h4 className="text-center">A nossa empresa se localiza no ajdkajdl</h4>
+                </div>
+            </div>
+
+            <div className="bg-purple-800 w-[180px] h-[220px] rounded-xl flex items-center" >
+            <div className="flex flex-col items-center">
+                    <img className="w-10" src="../../public/telefone.png" alt="" />
+                    <h2 className="text-center text-white">Localizacao</h2>
+                    <h4 className="text-center text-white">A nossa empresa se localiza no ajdkajdl</h4>
+                </div>
+            </div>
+        </div>
+
+        <div className="flex flex-row">
+        <div className="bg-purple-800 w-[180px] h-[220px] rounded-xl flex items-center mr-10" >
+            <div className="flex flex-col items-center">
+                    <img className="w-10" src="../../public/telefone.png" alt="" />
+                    <h2 className="text-center text-white">Localizacao</h2>
+                    <h4 className="text-center text-white">A nossa empresa se localiza no ajdkajdl</h4>
+                </div>
+            </div>
+            <div className="bg-white w-[180px] h-[220px] rounded-xl flex items-center" >
+                <div className="flex flex-col items-center">
+                    <img className="w-10" src="../../public/localizacao.png" alt="" />
+                    <h2 className="text-center">Localizacao</h2>
+                    <h4 className="text-center">A nossa empresa se localiza no ajdkajdl</h4>
+                </div>
+            </div>
+        </div>
+    </div>
         <form className="flex flex-col items-center justify-center" onSubmit={enviarEmail}>
-
+        <div className="mb-10">
+            <h1 className="text-3xl font-light text-center text-white">Nos envie suas duvidas!</h1>
+        </div>
             <input type="text" className="p-5 mb-5 rounded-xl w-[500px] h-12" placeholder="Digite seu nome" onChange={(enviar) => setName(enviar.target.value)} value={name}/>
           
             <input type="text" className="p-5 mb-5 rounded-xl w-[500px] h-12" placeholder="Digite seu email" onChange={(enviar) => setEmail(enviar.target.value)} value={email}/>
